2337f26d1906fb210a9bb07d29da3b10a5cc8676
Richard Lowe <ocdrichard@gmail.com>
2016-02-25 19:22:33 +0100

    Added STM32F7 support.

--- library/src/nrf24l01_hal.c
+++ library/src/nrf24l01_hal.c
@@ -27,6 +27,14 @@
 #include"stm32f0xx.h"
 #elif defined( STM32F1 )
 #include "stm32f1xx.h"
+#elif defined( STM32F2 )
+#include "stm32f2xx.h"
+#elif defined( STM32F3 )
+#include "stm32f3xx.h"
+#elif defined( STM32F4 )
+#include "stm32f4xx.h"
+#elif defined( STM32F7 )
+#include "stm32f7xx.h"
 #endif
 #endif
 
@@ -87,7 +95,8 @@ static void ( *spi_write_p )( unsigned int data_out );
 static unsigned char( *spi_read_p )( unsigned char dummy );
 static void( *spi_write_p )( unsigned char dataOut );
 #elif defined ( __GNUC__ )
-
+#if defined( STM32F0 ) || defined( STM32F1 ) || defined( STM32F2 ) || \
+	defined( STM32F3 ) || defined( STM32F4 ) || defined( STM32F7 )
 static HAL_StatusTypeDef ( *spi_write_p )( SPI_HandleTypeDef *hspi,
 										   uint8_t *pData,
 										   uint16_t Size,
@@ -96,7 +105,7 @@ static HAL_StatusTypeDef ( *spi_read_p )( SPI_HandleTypeDef *hspi,
 										  uint8_t *pData,
 										  uint16_t Size,
 										  uint32_t Timeout );
-
+#endif
 #endif
 
 #if defined( __MIKROC_PRO_FOR_ARM__ )   || \
@@ -109,11 +118,14 @@ static HAL_StatusTypeDef ( *spi_read_p )( SPI_HandleTypeDef *hspi,
 extern sfr sbit NRF_CS_PIN;
 extern sfr sbit NRF_CE_PIN;
 #elif defined( __GNUC__ )
-extern uint16_t NRF_CS_PIN;
-extern GPIO_TypeDef *NRF_CS_PORT;
-extern uint16_t NRF_CE_PIN;
-extern GPIO_TypeDef *NRF_CE_PORT;
-extern SPI_HandleTypeDef *NRF_SPI_PORT;
+	#if defined( STM32F0 ) || defined( STM32F1 ) || defined( STM32F2 ) || \
+		defined( STM32F3 ) || defined( STM32F4 ) || defined( STM32F7 )
+	extern uint16_t NRF_CS_PIN;
+	extern GPIO_TypeDef *NRF_CS_PORT;
+	extern uint16_t NRF_CE_PIN;
+	extern GPIO_TypeDef *NRF_CE_PORT;
+	extern SPI_HandleTypeDef *NRF_SPI_PORT;
+	#endif
 #endif
 
 
@@ -131,37 +143,60 @@ static void CE_PULSE( void );
 *******************************************************************************/
 static void CSN_LOW()
 {
-#if defined( __GNUC__ )
+	#if defined( __GNUC__ )
 	HAL_GPIO_WritePin( NRF_CS_PORT, NRF_CS_PIN, GPIO_PIN_RESET );
-#else
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     NRF_CS_PIN = 0;
-#endif
+	#endif
 }
-
 
 static void CSN_HIGH()
 {
-#if defined( __GNUC__ )
+	#if defined( __GNUC__ )
 	HAL_GPIO_WritePin( NRF_CS_PORT, NRF_CS_PIN, GPIO_PIN_SET );
-#else
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+		  defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     NRF_CS_PIN = 1;
 #endif
 }
 
 static void CE_LOW()
 {
-#if defined( __GNUC__ )
+	#if defined( __GNUC__ )
 	HAL_GPIO_WritePin( NRF_CE_PORT, NRF_CE_PIN, GPIO_PIN_RESET );
-#else
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     NRF_CE_PIN = 0;
-#endif
+	#endif
 }
 
 static void CE_HIGH()
 {
     #if defined( __GNUC__ )
 	HAL_GPIO_WritePin( NRF_CE_PORT, NRF_CE_PIN, GPIO_PIN_SET );
-    #else
+    #elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+	      defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     NRF_CE_PIN = 1;
     #endif
 }
@@ -173,11 +208,19 @@ static void CE_PULSE()
 {
     CE_HIGH();
     #if defined( __GNUC__ )
-    HAL_Delay( 10 );
-    #else
+		#if defined( STM32F0 ) || defined( STM32F1 ) || defined( STM32F2 ) || \
+			defined( STM32F3 ) || defined( STM32F4 ) || defined( STM32F7 )
+    	HAL_Delay( 1 );
+		#endif
+    #elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     Delay_10us();
     #endif
-
     CE_LOW();
 }
 
@@ -198,21 +241,24 @@ int nrf_hal_init( nrf_operation_mode_t mode )
     	CE_HIGH();
 
     #if defined( __MIKROC_PRO_FOR_ARM__ )   || \
-                defined( __MIKROC_PRO_FOR_AVR__ )   || \
-                defined( __MIKROC_PRO_FOR_PIC__ )   || \
-                defined( __MIKROC_PRO_FOR_PIC32__ ) || \
-                defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
-                defined( __MIKROC_PRO_FOR_8051__ )
-                spi_read_p = SPI_Rd_Ptr;
-                spi_write_p = SPI_Wr_Ptr;
+        defined( __MIKROC_PRO_FOR_AVR__ )   || \
+        defined( __MIKROC_PRO_FOR_PIC__ )   || \
+        defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+        defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+        defined( __MIKROC_PRO_FOR_8051__ )
+    spi_read_p = SPI_Rd_Ptr;
+    spi_write_p = SPI_Wr_Ptr;
     #elif defined( __MIKROC_PRO_FOR_FT90x__ )
-                spi_read_p = SPIM_Rd_Ptr;
-                spi_write_p = SPIM_Wr_Ptr;
+    spi_read_p = SPIM_Rd_Ptr;
+    spi_write_p = SPIM_Wr_Ptr;
     #elif defined( __GNUC__ )
-                spi_read_p = HAL_SPI_Receive;
-                spi_write_p = HAL_SPI_Transmit;
-    #endif
+		#if defined( STM32F0 ) || defined( STM32F1 ) || defined( STM32F2 ) || \
+			defined( STM32F3 ) || defined( STM32F4 ) || defined( STM32F7 )
+    	spi_read_p = HAL_SPI_Receive;
+    	spi_write_p = HAL_SPI_Transmit;
 
+    	#endif
+	#endif
     return ( spi_read_p == NULL || spi_write_p == NULL ) ? -1 : 0;
 }
 
@@ -220,13 +266,22 @@ int nrf_hal_init( nrf_operation_mode_t mode )
 void nrf_hal_listen()
 {
     CE_HIGH();
-#if defined( __GNUC__ )
-    HAL_Delay( 1 );
-#else
+	#if defined( __GNUC__ )
+		#if defined( STM32F0 ) || defined( STM32F1 ) || defined( STM32F2 ) || \
+			defined( STM32F3 ) || defined( STM32F4 ) || defined( STM32F7 )
+    	HAL_Delay( 1 );
+		#endif
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     Delay_50us();
     Delay_50us();
     Delay_50us();
-#endif
+	#endif
 }
 
 void nrf_hal_ignore()
@@ -237,16 +292,23 @@ void nrf_hal_ignore()
 
 uint8_t nrf_hal_write_reg( uint8_t address, uint8_t value )
 {
-    uint8_t status;
+    uint8_t status = 0;
 
     CSN_LOW();
-#if defined( __GNUC__ )
+	#if defined( __GNUC__ )
+    //TODO: Needs to be a read/write to get status
     spi_write_p( NRF_SPI_PORT, &address, 1, 0 );
     spi_write_p( NRF_SPI_PORT, &value, 1, 0 );
-#else
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     status = spi_read_p( address );
     spi_write_p( value );
-#endif
+	#endif
     CSN_HIGH();
 
     return status;
@@ -258,13 +320,19 @@ uint8_t nrf_hal_read_reg( uint8_t address )
     uint8_t read_value;
 
     CSN_LOW();
-#if defined( __GNUC__ )
+	#if defined( __GNUC__ )
     spi_write_p( NRF_SPI_PORT, &address, 1, 0 );
     spi_read_p( NRF_SPI_PORT, &read_value, 1, 0 );
-#else
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     spi_write_p( address );
     read_value = spi_read_p( 0x00 );
-#endif
+	#endif
     CSN_HIGH();
 
     return read_value;
@@ -273,14 +341,20 @@ uint8_t nrf_hal_read_reg( uint8_t address )
 int nrf_hal_write( uint8_t address, uint8_t *data_in, uint8_t count )
 {
     CSN_LOW();
-#if defined( __GNUC__ )
+	#if defined( __GNUC__ )
     spi_write_p( NRF_SPI_PORT, &address, 1, 0 );
     spi_write_p( NRF_SPI_PORT, data_in, count, 0 );
-#else
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     spi_write_p( address );
     while( count-- )
         spi_write_p( *( data_in++ ) );
-#endif
+	#endif
     CSN_HIGH();
 
     if( address == 0xA0 )
@@ -295,10 +369,16 @@ int nrf_hal_read( uint8_t address, uint8_t *data_out, uint8_t count )
         return 0;
 
     CSN_LOW();
-#if defined( __GNUC__ )
+	#if defined( __GNUC__ )
     spi_write_p( NRF_SPI_PORT, &address, 1, 0 );
     spi_read_p( NRF_SPI_PORT, data_out, count, 0 );
-#else
+	#elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     spi_write_p( address );
 
     while( count-- )
@@ -311,12 +391,21 @@ int nrf_hal_read( uint8_t address, uint8_t *data_out, uint8_t count )
 
 void nrf_hal_delay( uint16_t ms )
 {
-#if defined( __GNUC__ )
-    HAL_Delay( ms );
-#else
+	#if defined( __GNUC__ )
+		#if defined( STM32F0 ) || defined( STM32F1 ) || defined( STM32F2 ) || \
+			defined( STM32F3 ) || defined( STM32F4 ) || defined( STM32F7 )
+    	HAL_Delay( ms );
+		#endif
+    #elif defined( __MIKROC_PRO_FOR_ARM__ )   || \
+	      defined( __MIKROC_PRO_FOR_AVR__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC__ )   || \
+		  defined( __MIKROC_PRO_FOR_PIC32__ ) || \
+		  defined( __MIKROC_PRO_FOR_DSPIC__ ) || \
+		  defined( __MIKROC_PRO_FOR_8051__ )  || \
+		  defined( __MIKROC_PRO_FOR_FT90x__ )
     while( ms-- )
-        Delay_1ms();
-#endif
+    	  Delay_1ms();
+  #endif
 }
 
 
